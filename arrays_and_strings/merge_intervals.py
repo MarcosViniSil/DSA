4360f2c29cd0a4eaccaf5f21932b30d6dca5df27
MarcosViniSil <marcossilv203@gmail.com>
2025-12-18 20:12:02 -0300

    feat: add list[list[type]] into list of types

---
+++ arrays_and_strings/merge_intervals.py
@@ -0,0 +1,13 @@
+#Link to problem: https://leetcode.com/problems/merge-intervals/
+#Time complexity:
+#Space complexity: 
+                       
+class Solution:
+    def merge_intervals(self,intervals:list[list[int]]) -> list[list[int]]:
+        pass
+
+                                        
+solution = Solution()
+print(solution.merge_intervals([[1,3],[2,6],[8,10],[15,18]]))
+print(solution.merge_intervals([[1,4],[4,5]]))
+print(solution.merge_intervals([[4,7],[1,4]]))
