ed58b412dd7accb74f8127ac82395b177c224e34
MarcosViniSil <marcossilv203@gmail.com>
2026-01-07 17:46:14 -0300

    feat: add boilerplate to linked list

--- boilerPlate.py
+++ boilerPlate.py
@@ -5,6 +5,12 @@ import string
 
 CHARACTERS = string.ascii_letters
 
+from enum import Enum
+
+class TYPE_PROBLEM(Enum):
+    DEFAULT = 1
+    LINKED_LIST = 2
+
 VALID_RETURN_TYPES = {
     "int",
     "float",
@@ -62,23 +68,10 @@ sys.argv[n] -> argN
 def is_valid_return_type(type_str: str) -> bool:
     return type_str in VALID_RETURN_TYPES
 
-def extract_problem_name_from_url(url:str) -> str:
-    
-    url_parts_problem = url.split("problems")
-    problem_name_part = url_parts_problem[1]
-    
-    problem_name = problem_name_part.split("/")[1]
-    if problem_name is None or not problem_name:
-        raise ValueError("It was not possible to obtain problem name")
-    
-    return problem_name
 
-def createBoilerPlate(methodName:str,linkProblem:str,returnType:str,params:str,folder:str):
-    if not is_valid_return_type(returnType):
-        raise ValueError("Return type invalid")
-    
-    methodName = methodName.replace("-","_").replace(" ","")
-    template = Template("""#Link to problem: $linkProblem
+def getTemplate(type:TYPE_PROBLEM) -> Template:
+    if type == TYPE_PROBLEM.DEFAULT:
+        return Template("""#Link to problem: $linkProblem
 #Time complexity:
 #Space complexity: 
                        
@@ -91,6 +84,52 @@ solution = Solution()
 print(solution.$methodName())
 print(solution.$methodName())
 """)
+    
+    elif type == TYPE_PROBLEM.LINKED_LIST:
+        return Template("""#Link to problem: $linkProblem
+#Time complexity:
+#Space complexity: 
+                        
+from typing import Optional
+from help.linked_list_definition import ListNode
+from help.seed_linked_list import seed, print_linked_list
+                       
+class Solution:
+    def $methodName(self,$params) -> $returnType:
+        pass
+
+                                        
+solution = Solution()
+head = seed()
+head_after = solution.$methodName(head)
+print_linked_list(head_after)
+
+head = seed()
+head_after = solution.$methodName(head)
+print_linked_list(head_after)
+""")
+    
+def extract_problem_name_from_url(url:str) -> str:
+    
+    url_parts_problem = url.split("problems")
+    problem_name_part = url_parts_problem[1]
+    
+    problem_name = problem_name_part.split("/")[1]
+    if problem_name is None or not problem_name:
+        raise ValueError("It was not possible to obtain problem name")
+    
+    return problem_name
+
+def createBoilerPlate(methodName:str,linkProblem:str,returnType:str,params:str,folder:str):
+    type_problem = TYPE_PROBLEM.DEFAULT
+    if folder.startswith("linked_list"):
+        type_problem = TYPE_PROBLEM.LINKED_LIST
+
+    if not is_valid_return_type(returnType):
+        raise ValueError("Return type invalid")
+    
+    methodName = methodName.replace("-","_").replace(" ","")
+    template = getTemplate(type_problem)
 
     code = template.substitute(methodName=methodName,linkProblem=linkProblem,returnType=returnType,params=params)
 
